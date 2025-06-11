0806df19a457866cf81b43355ea250b9b57d0d28
brunohro <brunoh.rodrigues27@gmail.com>
2025-06-11 14:29:44 -0300

    Add imagens e novas divs na seção inicial

--- src/app/page.js
+++ src/app/page.js
@@ -13,8 +13,39 @@ export default function Home() {
             <Image src="/behance.svg" alt="github" width={32} height={32} />
           </div>
         </div>
-        <div>d2</div>
-        <div>D3</div>
+        {/* DIVISAO 1  */}
+        <div>
+          <Image
+            className="rounded-full"
+            src="/brunoOl.png"
+            alt="Bruno Henrique"
+            width={200}
+            height={200}
+          />
+        </div>
+        {/* DIVISAO 2  */}
+        <div>
+          <div className="flex items-center p-2 gap-2 justify-end text-purple-500/40">
+            <h1> Baixar CV</h1>
+            <div>
+              <Image
+                src="/ArrowLineDown.png"
+                alt="Arrow"
+                width={16}
+                height={16}
+              />
+            </div>
+          </div>
+          <div className="flex items-center border-2 p-2 rounded-2xl hover:cursor-pointer border-purple-500/40 gap-2 text-purple-500/40">
+            <Image
+              src="/WhatsappLogo.png"
+              alt="Whatsapp"
+              width={32}
+              height={32}
+            />
+            <h1>Vamos conversar</h1>
+          </div>
+        </div>
       </div>
     </div>
   );
